88611485bad21a443c2c9eae3fe0c325f40ea824
ekirlilar <55156419+ekirlilar@users.noreply.github.com>
2019-11-05 10:34:27 -0500

    New Game Adjustments
    Adding some figures to the game

---
+++ Kirlilar_E/conditionals.py
@@ -0,0 +1,18 @@
+# look at a temperature and figure out what state water is in - solid, liquid or gas
+
+   # set the temperature first - and turn our text input into a number => that's what int() does
+   temp = input("enter a temperature")
+
+   if temp < 4:
+   	print("water is frozen - a solid")
+elif temp > 4 and temp < 100:
+   		print (water is liquid)
+elif temp >= 100:
+   			print ("water is gas")
+else:
+	print("you didn't enter a number, try again")
+
+	if computer == player:
+		print("tie! no ones wins, play again")
+
+# need to check all of your conditions after checking for a tie
